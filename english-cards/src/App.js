e020c3b28d3ef30c3b88f85927c2a56876c16907
Aristarh Gran <awebdeff@gmail.com>
2022-10-22 21:04:25 +0300

    done

--- english-cards/src/App.js
+++ english-cards/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 class App extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
       id: 0,
@@ -14,11 +14,49 @@ class App extends React.Component {
     }
   }
 
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addCard() {
+    const newCard = {
+      id: 1 + this.state.id,
+      value: {
+        word: this.state.word.slice(),
+        translate: this.state.translate.slice(),
+        overturned: false
+      }
+    }
+
+    this.setState({
+      id: newCard.id,
+      word: '',
+      translate: '',
+      cards: [...this.state.cards, newCard]
+    })
+  }
+
+  turnCard(id) {
+    const cards = [...this.state.cards]
+
+    let index = cards.findIndex((card) => {
+      return card.id === id
+    })
+
+    cards[index].value.overturned = !cards[index].value.overturned
+
+    this.updateInput('cards', cards)
+  }
+
   render() {
     return (
       <div className='App'>
         <h1 className='app-title'>Карточки английских слов</h1>
         <div className='container'>
+          Добавить карточку
+          <br />
           <input
             type='text'
             placeholder='Введите слово'
@@ -31,9 +69,28 @@ class App extends React.Component {
             value={this.state.translate}
             onChange={(e) => this.updateInput('translate', e.target.value)}
           />
+          <button
+            className='btn add-btn'
+            onClick={() => this.addCard()}
+          >
+            Добавить
+          </button>
+          <div>
+            {this.state.cards.map(card => {
+              return (
+                <div
+                  key={card.id}
+                  className={'card' + (card.value.overturned ? ' overturned' : '')}
+                  onClick={() => this.turnCard(card.id)}
+                >
+                  {card.value.overturned ? card.value.translate : card.value.word}
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div >
-    );
+    )
   }
 }
 
